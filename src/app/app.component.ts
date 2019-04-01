466275cbece6a24aa2c80c0af9312fffa5da1f0a
André Pinto <andrefjpinto@gmail.com>
2019-04-01 09:07:06 +0100

    adds auth0 integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from './authentication.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cvbuilder-ui';
+
+  constructor(public auth: AuthenticationService) {
+    auth.handleAuthentication();
+  }
+
+  ngOnInit() {
+    if (this.auth.isAuthenticated()) {
+      this.auth.renewTokens();
+    }
+  }
+
+
 }
